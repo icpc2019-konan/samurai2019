6cf6cba95d3b182a33369c02972917f2e53e559b
Kazuhiro Seki <seki@konan-u.ac.jp>
2019-11-12 12:57:48 +0900

    Update

--- myplayers/random_player.py
+++ myplayers/random_player.py
@@ -1,17 +1,19 @@
 import os, random
 
-cnt = 0
 is_last = False
 
 while not is_last:
 
-    a = [input().split() for x in range(13)]
+    # read status
+    status = [input().split() for x in range(13)]
 
-    # last step
-    if int(a[2][0])+1 == int(a[3][0]):
+    # last step?
+    if int(status[2][0])+1 == int(status[3][0]):
         is_last = True
+
+    '''
+    need to change below
+    '''
 
     # choose random number from 0, 2, 4, 6
     print(random.randint(0, 3)*2, flush=True)
-
-
